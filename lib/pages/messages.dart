46d91d88c5416923b7e45f5d9a696891c9438f2b
Sandy_undefined <08sandysk@gmail.com>
2020-03-07 16:33:52 +0530

    Day-20 Chat UI Screen(progress).

--- lib/pages/messages.dart
+++ lib/pages/messages.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:college_services/services/usermanagement.dart';
 import 'package:flutter/material.dart';
+import 'Chat.dart';
 
 
 class Messages extends StatefulWidget {
@@ -42,10 +43,14 @@ class _MessagesState extends State<Messages> {
                   );
                 }
                 else{
-                  return ListView.builder(
+                  return ListView.separated(
+
                     padding: EdgeInsets.all(8.0),
                     itemBuilder: (context,index) => buildItem(context,snapshot.data.documents[index]),
                     itemCount: snapshot.data.documents.length,
+                    separatorBuilder: ((context,index){
+                      return Divider();
+                    }),
                   );
                 }
               },
@@ -59,15 +64,50 @@ class _MessagesState extends State<Messages> {
   Widget buildItem(BuildContext context,DocumentSnapshot document){
     if(document['Phone Number']== PhoneNumber){
       print("NO");
-      return Container();
+      return Container(
+        child: Row(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left:15.0),
+              child: RaisedButton(
+                shape:RoundedRectangleBorder( borderRadius: BorderRadius.circular(15.0),),
+                color: Color.fromRGBO(255,188,114, 1),
+                textColor: Colors.black,
+                padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 24.0),
+                child: Container(
+                  alignment: Alignment.center,
+                  width: 250,
+                  child: Text(
+                    "Sign Up",
+                    style: TextStyle(
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ),
+                onPressed: ()=>{},
+              ),
+            ),
+          ],
+        ),
+      );
     }
     else{
       print("yess!");
       return userFlag ? Container(
+
         child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: const BorderRadius.all(
+              Radius.circular(5.0),
+            ),
+            side: BorderSide(
+              color: Colors.grey,
+              width: .5,
+            ),
+          ),
           elevation: 2,
           child:Padding(
-            padding: EdgeInsets.only(left:10.0,top: 10),
+            padding: EdgeInsets.only(top: 10),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisSize: MainAxisSize.min,
@@ -87,12 +127,15 @@ class _MessagesState extends State<Messages> {
                         ),
                       ),
                       Padding(
-                        padding:  EdgeInsets.only(left:15),
+                        padding:  EdgeInsets.only(left:25),
                         child: Text(document["Name"],style: TextStyle(fontWeight: FontWeight.w800,fontSize: 14),),
                       ),
                     ],
                   ),
-                  onTap:() => print("Working"),
+                  onTap:()  {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => Chat(peerId: document.documentID, peerAvatar: document['Image Url'],
+                  )));
+                  },
                 ),
               ],
             ),
