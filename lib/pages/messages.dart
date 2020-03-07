669e4a3d9bbfb2d59fb9c790833717267830cabb
Sandy_undefined <08sandysk@gmail.com>
2020-03-07 14:32:35 +0530

    Day-20 Message Screen UI(Completed).

--- lib/pages/messages.dart
+++ lib/pages/messages.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:college_services/services/usermanagement.dart';
 import 'package:flutter/material.dart';
 
 
@@ -7,23 +9,97 @@ class Messages extends StatefulWidget {
 }
 
 class _MessagesState extends State<Messages> {
+  bool userFlag = false;
+  var users;
+  String PhoneNumber;
+
+  @override
+  void initState() {
+    super.initState();
+    UserManagement().getData().then((results) {
+      setState(() {
+        userFlag = true;
+        users = results;
+        PhoneNumber = users['Phone Number'];
+      });
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Messages'),
       ),
-      body: Container(
-        child: Center(
-          child: Text(
-            'Messages ',
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
+      body: Stack(
+        children:<Widget>[
+          Container(
+            child: StreamBuilder(
+              stream: Firestore.instance.collection('users').snapshots(),
+              builder: (context,snapshot){
+                if(!snapshot.hasData){
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                else{
+                  return ListView.builder(
+                    padding: EdgeInsets.all(8.0),
+                    itemBuilder: (context,index) => buildItem(context,snapshot.data.documents[index]),
+                    itemCount: snapshot.data.documents.length,
+                  );
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildItem(BuildContext context,DocumentSnapshot document){
+    if(document['Phone Number']== PhoneNumber){
+      print("NO");
+      return Container();
+    }
+    else{
+      print("yess!");
+      return userFlag ? Container(
+        child: Card(
+          elevation: 1,
+          child:Padding(
+            padding: EdgeInsets.only(left:10.0,top: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                InkWell(
+                  child: new Row(
+                    children: <Widget>[
+                      Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: NetworkImage(document["Image Url"]),
+                            fit: BoxFit.cover,
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(50.5)),
+                        ),
+                      ),
+                      Padding(
+                        padding:  EdgeInsets.only(left:15),
+                        child: Text(document["Name"],style: TextStyle(fontWeight: FontWeight.w800,fontSize: 14),),
+                      ),
+                    ],
+                  ),
+                  onTap:() => print("Working"),
+                ),
+              ],
             ),
           ),
         ),
-      ),
-    );
+      )
+          : new Container();
+    }
   }
 }
